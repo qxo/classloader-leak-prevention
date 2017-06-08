62495c0a91a5f325851d0f827967220af9b862c7
qxo <49526356@qq.com>
2017-06-08 09:36:28 +0800

    dummy

--- classloader-leak-prevention/classloader-leak-prevention-core/src/main/java/se/jiderhamn/classloader/leak/prevention/cleanup/ResourceBundleCleanUp.java
+++ classloader-leak-prevention/classloader-leak-prevention-core/src/main/java/se/jiderhamn/classloader/leak/prevention/cleanup/ResourceBundleCleanUp.java
@@ -15,34 +15,37 @@ import se.jiderhamn.classloader.leak.prevention.ClassLoaderPreMortemCleanUp;
  * @author Mattias Jiderhamn
  */
 public class ResourceBundleCleanUp implements ClassLoaderPreMortemCleanUp {
+	
+  private static Field loaderRefField = null;
+	
   @Override
   public void cleanUp(ClassLoaderLeakPreventor preventor) {
     try {
-      try { // First try Java 1.6 method
+      try { 
+//    	  
+//	      ResourceBundle nullBundle = preventor.getStaticFieldValue(ResourceBundle.class,"NONEXISTENT_BUNDLE");
+//	      if( nullBundle != null){
+//	      	final Object cacheKey = preventor.getFieldValue(nullBundle, "cacheKey");
+//	      	if( cacheKey != null){
+//	      		 preventor.info("Removing ResourceBundle NONEXISTENT_BUNDLE cacheKey: " + cacheKey);
+//	      		 clearLoaderRef(preventor,  cacheKey);
+//	      	}
+//	      }  
+    	// First try Java 1.6 method
         final Method clearCache16 = ResourceBundle.class.getMethod("clearCache", ClassLoader.class);
-        preventor.debug("Since Java 1.6+ is used, we can call " + clearCache16);
+        preventor.info("Since Java 1.6+ is used, we can call " + clearCache16);
         clearCache16.invoke(null, preventor.getClassLoader());
+
       }
       catch (NoSuchMethodException e) {
         // Not Java 1.6+, we have to clear manually
         final Map<?,?> cacheList = preventor.getStaticFieldValue(ResourceBundle.class, "cacheList"); // Java 5: SoftCache extends AbstractMap
         final Iterator<?> iter = cacheList.keySet().iterator();
-        Field loaderRefField = null;
         while(iter.hasNext()) {
           Object key = iter.next(); // CacheKey
-          
-          if(loaderRefField == null) { // First time
-            loaderRefField = key.getClass().getDeclaredField("loaderRef");
-            loaderRefField.setAccessible(true);
+          if( clearLoaderRef(preventor,  key)){
+        	  iter.remove();
           }
-          WeakReference<ClassLoader> loaderRef = (WeakReference<ClassLoader>) loaderRefField.get(key); // LoaderReference extends WeakReference
-          ClassLoader classLoader = loaderRef.get();
-          
-          if(preventor.isClassLoaderOrChild(classLoader)) {
-            preventor.info("Removing ResourceBundle from cache: " + key);
-            iter.remove();
-          }
-          
         }
       }
     }
@@ -52,4 +55,20 @@ public class ResourceBundleCleanUp implements ClassLoaderPreMortemCleanUp {
     
     // (CacheKey of java.util.ResourceBundle.NONEXISTENT_BUNDLE will point to first referring classloader...)
   }
+
+private boolean clearLoaderRef(ClassLoaderLeakPreventor preventor,  Object key)
+		throws NoSuchFieldException, IllegalAccessException {
+	if(loaderRefField == null) { // First time
+	    loaderRefField = key.getClass().getDeclaredField("loaderRef");
+	    loaderRefField.setAccessible(true);
+	  }
+	  WeakReference<ClassLoader> loaderRef = (WeakReference<ClassLoader>) loaderRefField.get(key); // LoaderReference extends WeakReference
+	  ClassLoader classLoader = loaderRef.get();
+	  
+	  if(preventor.isClassLoaderOrChild(classLoader)) {
+	    preventor.info("Removing ResourceBundle from cache: " + key);
+	    return true;
+	  }
+	  return false;
+}
 }
